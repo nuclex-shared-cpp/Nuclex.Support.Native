563a8684c98527425f31dbd170789e6d7ed5f726
Markus Ewald <cygon@nuclex.org>
2021-01-19 14:22:00 +0000

    Merged Linux-specific timestamp code with Posix-specific one after my distro's compiler complained about timeb being deprecated; implemented RollingLogger::GetLines() method; implemented RollingLogger::Clear() method; wrote a bunch of unit tests for various aspects of the rolling logger
    git-svn-id: file:///srv/devel/repo-conversion/nusu@160 d2e56fa2-650e-0410-a79f-9358c0239efd

--- Source/Text/RollingLogger.cpp
+++ Source/Text/RollingLogger.cpp
@@ -28,10 +28,10 @@ License along with this library
 #define VC_EXTRALEAN
 #define NO_MINMAX
 #include <Windows.h> // for GetSystemTime()
-#elif defined(NUCLEX_SUPPORT_LINUX)
-#include <sys/timeb.h> // ::timeb, ::ftime(), ::gmtime_r()
 #else
 #include <ctime> // for ::timespec, ::clock_gettime() and ::gmtime_r()
+#include <cerrno> // for ::errno
+#include "../Helpers/PosixApi.h" // for strerror() wrapper
 #endif
 
 #include <cassert> // for assert()
@@ -52,6 +52,7 @@ namespace {
     { '4', '2' }, { '4', '3' }, { '4', '4' }, { '4', '5' }, { '4', '6' }, { '4', '7' },
     { '4', '8' }, { '4', '9' }, { '5', '0' }, { '5', '1' }, { '5', '2' }, { '5', '3' },
     { '5', '4' }, { '5', '5' }, { '5', '6' }, { '5', '7' }, { '5', '8' }, { '5', '9' }
+    //{ '6', '0' }, { '6', '1' }, { '6', '2' }, { '6', '3' }, { '6'}
   };
 
   // ------------------------------------------------------------------------------------------- //
@@ -90,14 +91,14 @@ namespace Nuclex { namespace Support { namespace Text {
   ) :
     nextLineIndex(0),
     oldestLineIndex(0),
-    lines(historyLineCount),
+    lines(historyLineCount + 1), // +1 for the line being formed (it's constructed in place)
     currentLine(nullptr),
     indentationCount(0) {
     assert((historyLineCount >= 1) && u8"History line count must be at least one line");
 
     // Reserve memory on all lines so we have one up-front allocation that will hopefully
     // stay the only one (assuming our user doesn't build overlong lines)
-    for(std::size_t index = 0; index < historyLineCount; ++index) {
+    for(std::size_t index = 0; index < historyLineCount + 1; ++index) {
       this->lines[index].reserve(lineSizeHint);
     }
 
@@ -201,19 +202,48 @@ namespace Nuclex { namespace Support { namespace Text {
   // ------------------------------------------------------------------------------------------- //
 
   void RollingLogger::Append(const char *buffer, std::size_t count) {
-
+    this->currentLine->append(buffer, count);
   }
 
   // ------------------------------------------------------------------------------------------- //
 
   void RollingLogger::Clear() {
-
+    assert(
+      (this->indentationCount == 0) && u8"Indentation should be zero when calling Clear()"
+    );
+    this->oldestLineIndex = 0;
+    this->nextLineIndex = 0;
+    this->currentLine = &this->lines[0];
+    this->indentationCount = 0;
   }
 
   // ------------------------------------------------------------------------------------------- //
 
   std::vector<std::string> RollingLogger::GetLines() const {
+    if(this->oldestLineIndex == this->nextLineIndex) {
+      return std::vector<std::string>();
+    }
 
+    std::vector<std::string> orderedLines;
+    if(this->oldestLineIndex < this->nextLineIndex) {
+      orderedLines.reserve(this->nextLineIndex - this->oldestLineIndex);
+      for(std::size_t index = this->oldestLineIndex; index < this->nextLineIndex; ++index) {
+        orderedLines.push_back(this->lines[index]);
+      }
+    } else {
+      {
+        std::size_t historyLineCount = this->lines.size();
+        orderedLines.reserve(historyLineCount - this->oldestLineIndex + this->nextLineIndex);
+        for(std::size_t index = this->oldestLineIndex; index < historyLineCount; ++index) {
+          orderedLines.push_back(this->lines[index]);
+        }
+      }
+      for(std::size_t index = 0; index < this->nextLineIndex; ++index) {
+        orderedLines.push_back(this->lines[index]);
+      }
+    }
+
+    return orderedLines;
   }
 
   // ------------------------------------------------------------------------------------------- //
@@ -237,6 +267,8 @@ namespace Nuclex { namespace Support { namespace Text {
     assert((line.length() >= 12) && u8"Line is long enough to hold the current time");
 
 #if defined(NUCLEX_SUPPORT_WIN32)
+
+    // Interestingly, Microsoft's GetSystemTime() has no error return.
     ::SYSTEMTIME splitUtcTime;
     ::GetSystemTime(&splitUtcTime);
     {
@@ -253,46 +285,34 @@ namespace Nuclex { namespace Support { namespace Text {
       lexical_append(currentCharacter + 9, 3, splitUtcTime.wMilliseconds);
       currentCharacter[12] = ' ';
     }
-#elif defined(NUCLEX_SUPPORT_LINUX)
-    ::timeb time;
-    {
-      int result = ::ftime(&time);
-      if(result != 0) {
-        // Handle posix error
-      }
-    }
 
-    ::tm splitUtcTime;
-    ::gmtime_r(&time.time, &splitUtcTime);
-    {
-      std::string::value_type *currentCharacter = line.data();
-      currentCharacter[0] = TimestampDigits[splitUtcTime.tm_hour][0];
-      currentCharacter[1] = TimestampDigits[splitUtcTime.tm_hour][1];
-      currentCharacter[2] = ':';
-      currentCharacter[3] = TimestampDigits[splitUtcTime.tm_min][0];
-      currentCharacter[4] = TimestampDigits[splitUtcTime.tm_min][1];
-      currentCharacter[5] = ':';
-      currentCharacter[6] = TimestampDigits[splitUtcTime.tm_sec][0];
-      currentCharacter[7] = TimestampDigits[splitUtcTime.tm_sec][1];
-      currentCharacter[8] = '.';
-      lexical_append(line.data() + 9, 3, time.millitm);
-      currentCharacter[12] = ' ';
-    }
-#else // Posix
+#else // Posix and Linux through Posix
+
+    // Obtain the current wall clock time. This clock /may/ skip or jump backwards if time
+    // is synchronized by, for example, an NTP daemon. For logging, this doesn't matter much
+    // as the lines are still ordered and the log isn't intended for benchmarking.
     ::timespec time;
     {
-      int result = ::clock_gettime(CLOCK_MONOTONIC, &time);
+      int result = ::clock_gettime(CLOCK_REALTIME, &time);
       if(result != 0) {
-        // Handle posix error
+        int errorNumber = errno;
+        Helpers::PosixApi::ThrowExceptionForSystemError(
+          u8"Could not obtain the current wall clock via ::clock_gettime(CLOCK_REALTIME...)",
+          errorNumber
+        );
       }
     }
 
+    // Turn the 'seconds since the epoch' value into hours, minutes and seconds in UTC
+    // According to docs, time.tv_sec should always be ::time_t, but some Posix implementations
+    // have a lot of defines going on and 32/64 bit variants, so we're being explicit here.
     ::tm splitUtcTime;
     {
       ::time_t secondsSinceEpoch = static_cast<::time_t>(time.tv_sec);
       ::gmtime_r(&secondsSinceEpoch, &splitUtcTime);
     }
 
+    // Finally, form the time stamp in the log line
     {
       const std::size_t nanoSecondsPerMilliSecond = 1000000U;
       std::string::value_type *currentCharacter = line.data();
@@ -308,6 +328,7 @@ namespace Nuclex { namespace Support { namespace Text {
       lexical_append(currentCharacter + 9, 3, time.tv_nsec / nanoSecondsPerMilliSecond);
       currentCharacter[12] = ' ';
     }
+
 #endif
   }
 
--- Tests/Text/RollingLoggerTest.cpp
+++ Tests/Text/RollingLoggerTest.cpp
@@ -29,15 +29,6 @@ namespace Nuclex { namespace Support { namespace Text {
 
   // ------------------------------------------------------------------------------------------- //
 
-  TEST(RollingLoggerTest, CanDetectIfLoggingMethodsOverridden) {
-    EXPECT_FALSE(Logger::Null.IsLogging());
-
-    Logger logger;
-    EXPECT_TRUE(logger.IsLogging());
-  }
-
-  // ------------------------------------------------------------------------------------------- //
-
   TEST(RollingLoggerTest, RollingLoggerCanBeDefaultConstructed) {
     EXPECT_NO_THROW(
       RollingLogger logger;
@@ -46,9 +37,146 @@ namespace Nuclex { namespace Support { namespace Text {
 
   // ------------------------------------------------------------------------------------------- //
 
-  TEST(RollingLoggerTest, InformationsCanBeLogged) {
+  TEST(RollingLoggerTest, IsLoggingReturnsTrue) {
+    Logger logger;
+    EXPECT_TRUE(logger.IsLogging());
+
+    // Negative test
+    EXPECT_FALSE(Logger::Null.IsLogging());
+  }
+
+  // ------------------------------------------------------------------------------------------- //
+
+  TEST(RollingLoggerTest, LoggerAcceptsPlainLogEntries) {
     RollingLogger logger;
+    EXPECT_NO_THROW(
+      logger.Inform(u8"This is a harmless message providing information");
+      logger.Warn(u8"This is a warning indicating something is not optimal");
+      logger.Complain(u8"This is an error and some action has failed completely");
+    );
+  }
+
+  // ------------------------------------------------------------------------------------------- //
+
+  TEST(RollingLoggerTest, LogHistoryCanBeExtracted) {
+    RollingLogger logger;
+
+    std::vector<std::string> history = logger.GetLines();
+    EXPECT_EQ(history.size(), 0);
+
     logger.Inform(u8"This is a harmless message providing information");
+    history = logger.GetLines();
+    EXPECT_EQ(history.size(), 1);
+    EXPECT_TRUE(history[0].find(u8"This is a harmless message") != std::string::npos);
+
+    logger.Warn(u8"This is a warning indicating something is not optimal");
+    history = logger.GetLines();
+    EXPECT_EQ(history.size(), 2);
+    EXPECT_TRUE(history[1].find(u8"This is a warning") != std::string::npos);
+
+    logger.Complain(u8"This is an error and some action has failed completely");
+    history = logger.GetLines();
+    EXPECT_EQ(history.size(), 3);
+    EXPECT_TRUE(history[2].find(u8"This is an error") != std::string::npos);
+  }
+
+  // ------------------------------------------------------------------------------------------- //
+
+  TEST(RollingLoggerTest, LogHistoryKeepsMostRecentLines) {
+    RollingLogger logger(2U); // 2 lines history length
+
+    logger.Inform(u8"First line");
+    logger.Inform(u8"Second line");
+    logger.Inform(u8"Third line");
+
+    std::vector<std::string> history = logger.GetLines();
+    EXPECT_EQ(history.size(), 2);
+    EXPECT_TRUE(history[0].find(u8"Second line") != std::string::npos);
+    EXPECT_TRUE(history[1].find(u8"Third line") != std::string::npos);
+  }
+
+  // ------------------------------------------------------------------------------------------- //
+
+  TEST(RollingLoggerTest, LogHistoryCanBeCleared) {
+    RollingLogger logger;
+
+    logger.Inform(u8"Test");
+    logger.Inform(u8"Test");
+    logger.Clear();
+    logger.Inform(u8"First line");
+
+    std::vector<std::string> history = logger.GetLines();
+    EXPECT_EQ(history.size(), 1);
+    EXPECT_TRUE(history[0].find(u8"First line") != std::string::npos);
+  }
+
+  // ------------------------------------------------------------------------------------------- //
+
+  TEST(RollingLoggerTest, LoggerCanAppendIntegers) {
+    RollingLogger logger;
+
+    logger.Append(12345);
+    logger.Append(u8"Hello");
+    logger.Append(54321);
+    logger.Inform(u8"World");
+
+    std::vector<std::string> history = logger.GetLines();
+    EXPECT_EQ(history.size(), 1);
+    EXPECT_TRUE(history[0].find(u8"12345Hello54321World") != std::string::npos);
+  }
+
+  // ------------------------------------------------------------------------------------------- //
+
+  TEST(RollingLoggerTest, LoggerCanAppendFloatingPointValues) {
+    RollingLogger logger;
+
+    logger.Append(1.25f);
+    logger.Append(u8"Hello");
+    logger.Append(0.875);
+    logger.Inform(u8"World");
+
+    std::vector<std::string> history = logger.GetLines();
+    EXPECT_EQ(history.size(), 1);
+    EXPECT_TRUE(history[0].find(u8"1.25Hello0.875World") != std::string::npos);
+  }
+
+  // ------------------------------------------------------------------------------------------- //
+
+  TEST(RollingLoggerTest, LogLinesCanBeIndented) {
+    RollingLogger logger;
+
+    logger.Inform(u8"Saving configuration {");
+    {
+      Logger::IndentationScope configurationLogScope(logger);
+
+      logger.Append(u8"ResolutionX = ");
+      logger.Append(1920);
+      logger.Inform(std::string());
+
+      logger.Append(u8"ResolutionY = ");
+      logger.Append(1080);
+      logger.Inform(std::string());
+    }
+
+    logger.Inform(u8"}");
+
+    std::vector<std::string> history = logger.GetLines();
+    EXPECT_EQ(history.size(), 4);
+
+    std::string::size_type logTextStartColumn = history[0].find(u8"Saving configuration {");
+    ASSERT_NE(logTextStartColumn, std::string::npos);
+
+    EXPECT_EQ(history[0].at(logTextStartColumn + 0), 'S'); // ...aving Configuration
+
+    EXPECT_EQ(history[1].at(logTextStartColumn + 0), ' ');
+    EXPECT_EQ(history[1].at(logTextStartColumn + 1), ' ');
+    EXPECT_EQ(history[1].at(logTextStartColumn + 2), 'R'); // ...esolutionX
+
+    EXPECT_EQ(history[2].at(logTextStartColumn + 0), ' ');
+    EXPECT_EQ(history[2].at(logTextStartColumn + 1), ' ');
+    EXPECT_EQ(history[2].at(logTextStartColumn + 2), 'R'); // ...resolutionY
+
+    EXPECT_EQ(history[3].at(logTextStartColumn + 0), '}');
   }
 
   // ------------------------------------------------------------------------------------------- //
