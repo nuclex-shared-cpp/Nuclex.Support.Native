5c1a70ba9832add907a754445b701ac371ae0db7
Markus Ewald <cygon@nuclex.org>
2021-04-13 11:57:25 +0000

    Changed Gate header missing form previous commit
    git-svn-id: file:///srv/devel/repo-conversion/nusu@267 d2e56fa2-650e-0410-a79f-9358c0239efd

--- Include/Nuclex/Support/Threading/Gate.h
+++ Include/Nuclex/Support/Threading/Gate.h
@@ -31,6 +31,21 @@ namespace Nuclex { namespace Support { namespace Threading {
   // ------------------------------------------------------------------------------------------- //
 
   /// <summary>Lets threads through only if opened</summary>
+  /// <remarks>
+  ///   <para>
+  ///     This is one of the simplest thread synchronization primitives. It will
+  ///     simply block all threads while it is closed and let all threads through
+  ///     while it is open.
+  ///   </para>
+  ///   <para>
+  ///     It can be used in place of a reverse counting semaphore to wait for multiple
+  ///     threads to complete their work or to launch multiple threads if you
+  ///     intentionally want to construct a high-contention situation.
+  ///   </para>
+  ///   <para>
+  ///     To Windows and .NET developers, it is known as a &quot;ManualResetEvent&quot;
+  ///   </para>
+  /// </remarks>
   class Gate {
 
     /// <summary>Initializes a new gate in an open or closed state</summary>
@@ -79,7 +94,7 @@ namespace Nuclex { namespace Support { namespace Threading {
     /// <summary>Accesses the platform dependent implementation data container</summary>
     /// <returns>A reference to the platform dependent implementation data</returns>
     private: PlatformDependentImplementationData &getImplementationData();
-    private: union {
+    private: union alignas(4) {
       /// <summary>Platform dependent process and file handles used for the process</summary>
       PlatformDependentImplementationData *implementationData;
       /// <summary>Used to hold the platform dependent implementation data if it fits</summary>
@@ -87,9 +102,11 @@ namespace Nuclex { namespace Support { namespace Threading {
       ///   Small performance / memory fragmentation improvement.
       ///   This avoids a micro-allocation for the implenmentation data structure in most cases.
       /// </remarks>
-#if defined(NUCLEX_SUPPORT_WIN32)
-      unsigned char implementationDataBuffer[8];
-#else // Posix and Linux
+#if defined(NUCLEX_SUPPORT_LINUX)
+      unsigned char implementationDataBuffer[4];
+#elif defined(NUCLEX_SUPPORT_WIN32)
+      unsigned char implementationDataBuffer[sizeof(std::size_t)]; // matches HANDLE size
+#else // Posix
       unsigned char implementationDataBuffer[96];
 #endif
     };
