b8200fd7a372d165586a20adf8cccd1e793cc11e
Markus Ewald <cygon@nuclex.org>
2023-08-19 10:52:38 +0000

    Retired the MoodyCamel containers (they're not bad, but they do their own Linux/Windows API calls and if I put actual lock-free containers in Nuclex.Support in the future, I want simpler, more portable containers)
    git-svn-id: file:///srv/devel/repo-conversion/nusu@475 d2e56fa2-650e-0410-a79f-9358c0239efd

--- Include/Nuclex/Support/Collections/ConcurrentSet.h
+++ Include/Nuclex/Support/Collections/ConcurrentSet.h
@@ -23,106 +23,44 @@ License along with this library
 
 #include "Nuclex/Support/Config.h"
 
-#include <atomic>
-
-#if 0
-
 namespace Nuclex { namespace Support { namespace Collections {
 
   // ------------------------------------------------------------------------------------------- //
 
-  template<typename TElement>
+  /// <summary>Set that can safely be used from multiple threads</summary>
+  /// <typeparam name="TKey">Type of the key the set will keep track of</typeparam>
+  template<typename TKey>
   class ConcurrentSet {
 
-    #pragma region struct Node
+    /// <summary>Destroys the concurrent set</summary>
+    public: virtual ~ConcurrentSet() = default;
 
-    /// <summary>Stores a list entry</summary>
-    private: struct Node {
-      /// <summary>Initializes a new list node</summary>
-      /// <param name="element">Element the node will carry</param>
-      public: Node(const TElement &element) :
-        Next(nullptr),
-        ReferenceCount(1),
-        Element(element) {}
+    /// <summary>Tries to insert a key into the set in a thread-safe manner</summary>
+    /// <param name="key">Key that will be inserted into the set</param>
+    /// <returns>True if the key was inserted, false if the key already existed</returns>
+    public: virtual bool TryInsert(const TKey &key) = 0;
 
-      /// <summary>Points to the next element in the list</summary>
-      public: std::atomic<Node *> Next;
-      // <summary>Number of references that exist to this node</summary>
-      //public: std::atomic_size_t ReferenceCount;
-      /// <summary>Stores the element itself</summary>
-      public: TElement Element;
-    };
+    /// <summary>Tries to remove a key from the set</summary>
+    /// <param name="key">Key that will be removed from the set</param>
+    /// <returns>
+    ///   True if the key was removed from the set, false if the key didn't exist (anymore?)
+    /// </returns>
+    public: virtual bool TryRemove(const TKey &key) = 0;
 
-    #pragma endregion // struct Node
+    /// <summary>Counts the numebr of keys currently in the set</summary>
+    /// <returns>
+    ///   The approximate number of keys that had been in the set during the call
+    /// </returns>
+    public: virtual std::size_t Count() const = 0;
 
-    /// <summary>Initializes a new set</summary>
-    public: ConcurrentSet() :
-      head(nullptr) {}
-
-    /// <summary>Adds an element to the set</summary>
-    /// <param name="element">Element that will be added to the set</param>
-    public: void Add(const TElement &element) {
-      Node *newNode = new Node(element);
-
-      // Acquire the current list head so it can be replaced
-      Node *currentHead = this->head.load(std::memory_order_acquire);
-
-      bool wasExchanged;
-      do {
-        // If we successfully replace the list head (and nobody snatched it away),
-        // the next pointer needs to point to the old head node.
-        newNode->Next.store(std::memory_order_release);
-
-        // Only replace the head if it was still what we prepared the new node for.
-        // If the head was replaced by something else in the meantime, it will be
-        // stored in currentHead, so we can try again in the next loop.
-        wasExchanged = this->head.compare_exchange_strong(
-          currentHead, nodeToInsert, std::memory_order_acq_rel
-        );
-      } while(!wasExchanged);
-
-      ++count;
-    }
-
-    /// <summary>Removes the specified
-    public: bool Remove(const TElement &element) {
-      this->threadCount.fetch_add(1, std::memory_order_acquire);
-
-      Node *currentHead = this->head.load(std::memory_order_acquire);
-      if(currentHead->Element == element) {
-        for(;;) {
-          //this->head.fetch_add();
-        }
-      }
-      // Uh-oh. I don't have the slightest clue how I could safely delete the node
-      //
-      // 1. Increment thread count
-      // 2. Find element to remove
-      // 3. Replace previous node's next pointer (CAS loop)
-      // 4. Add removed element to garbage list
-      // 5. Decrement thread count
-      // 6. If thread count was 0, guarantee that no thread was accessing any node
-      //    contained in the garbage list at the time of the call
-      // 6a. Replace garbage head with garbage next (CAS)
-      // 6b. If CAS failed, return (another thread is adding to the garbage)
-      //     If CAS succeeded, delete extracted garbage node, go to 6a.
-    }
-
-    /// <summary>First node in the linked list</summary>
-    private: std::atomic<Node *> head;
-    /// <summary>Garbage list, to be freed when the last thread leaves</summary>
-    private: std::atomic<Node *> garbage;
-    /// <summary>Stores the approximate number of elements in the list</summary>
-    private: std::atomic_size_t count;
-    /// <summary>Number of threads currently using the set</summary>
-    private: std::atomic_size_t threadCount;
+    /// <summary>Checks if the set is empty</summary>
+    /// <returns>True if the set had been empty during the call</returns>
+    public: virtual bool IsEmpty() const = 0;
 
   };
 
   // ------------------------------------------------------------------------------------------- //
 
 }}} // namespace Nuclex::Support::Collections
-
-#endif // 0
 
 #endif // NUCLEX_SUPPORT_COLLECTIONS_CONCURRENTSET_H
